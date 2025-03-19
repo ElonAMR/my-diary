a08c66a7885e093556a4fade94a94dd040e5d883
אלון עמרם <elonamrammm1234@gmail.com>
2025-03-19 19:22:31 +0200

    install library ejs & created and render first page

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ const express = require('express');
 const app= express();
 app.use(express.json());
 
+
 const bodyParser=require('body-parser');
 const path =require('path');
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.set("view engine","ejs");
+app.set("views" , path.join(__dirname,"./views"));
+
+
+
 app.listen(port , () => {
     console.log(`Now Listening on port http://localhost:${port}`);
 })
+
+
+app.get( '/' , (req,res) =>{
+    res.render( "index" , {} );
+});
