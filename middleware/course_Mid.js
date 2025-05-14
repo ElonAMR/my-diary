72c7d4e9db45b7db017a26f9cd07fc4da4d1914d
Elon Amram <elonamrammm1234@gmail.com>
2025-05-14 17:48:01 +0300

    add function Update Course

--- middleware/course_Mid.js
+++ middleware/course_Mid.js
@@ -11,3 +11,24 @@ async function addCourse(req,res,next){
 
     next();
 }
+
+
+async function UpdateCourse(req,res,next){
+    let id = parseInt(req.params.id);
+    if (id <= 0){
+        req.GoodOne=false;
+        return next();
+    }
+    req.GoodOne=true;
+    let name = addSlashes(req.body.name);
+    let Query=`UPDATE courses SET name='${name}' WHERE id='${id}'`;
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+    } catch (err) {
+        console.log(err);
+    }
+
+    next();
+}
