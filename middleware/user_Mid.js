d7d1005c22c64307d4ce3a72ca3244e26372dd3f
Elon Amram <elonamrammm1234@gmail.com>
2025-05-16 17:24:55 +0300

    added function for add user

--- middleware/user_Mid.js
+++ middleware/user_Mid.js
@@ -14,3 +14,28 @@ async function CheckLogin(uname,passwd){
     return (rows.length > 0);
 }
 
+
+async function AddUser(req,res,next){
+    let name    = (req.body.name   !== undefined) ? addSlashes(req.body.name      ) : "";
+    let uname   = (req.body.uname  !== undefined) ? addSlashes(req.body.uname     ) : "";
+    let passwd  = (req.body.passwd !== undefined) ?            req.body.passwd      : "";
+    let enc_pass = md5("A"+passwd);
+    let email   = (req.body.email  !== undefined) ? addSlashes(req.body.email     ) : "";
+    let type_id = (req.body.type_id!== undefined) ?     Number(req.body.type_id   ) : -1;
+    let tz      = (req.body.tz     !== undefined) ? addSlashes(req.body.tz        ) : "";
+
+    let Query="INSERT INTO users";
+    Query +="( `name`, `uname`, `passwd`, `email`, `type_id`, `tz`)";
+    Query +=" VALUES ";
+    Query +=`( '${name}', '${uname}', '${enc_pass}', '${email}', '${type_id}', '${tz}')`;
+    // console.log(__filename,Query);
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+    } catch (err) {
+        console.log(err);
+    }
+
+    next();
+}
